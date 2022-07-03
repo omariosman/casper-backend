19e60ad56f1c389a4b3e5d23005667259093a900
omar <omarosman23@aucegypt.edu>
2022-07-03 18:32:08 +0200

    finished

--- smart_contract/views.py
+++ smart_contract/views.py
@@ -68,97 +68,112 @@ def _get_deploy(args: argparse.Namespace, cp1: PrivateKey, cp2: PublicKey) -> De
 
     return deploy
 
+qr_code_dict = {"123": 0, "456": 0}
 def send_address(request):
     #Write your script here
     
     data = JSONParser().parse(request)
+    qr_code = data["qrCode"]
+    pub_address = data["publicAddress"]
+    with open('E:\Blockchain\Casper\public_keys\omar_pb_key.txt', 'w') as f:
+        f.write(pub_address)
+
+    qr_code_list = ["123", "456"]
     
-    address = data["publicAddress"]
-    pub_address = address["publicAddress"]
-    _ARGS = argparse.ArgumentParser("Illustration of how to execute native transfers.")
+    if qr_code in qr_code_list and qr_code_dict[qr_code] == 0:
+        qr_code_dict[qr_code] = 1
+        _ARGS = argparse.ArgumentParser("Illustration of how to execute native transfers.")
 
-    _ARGS.add_argument(
-        "--cp1-secret-key-path",
-        default="E:\Blockchain\Casper\wallet_key\TreasuryAccount_secret_key.pem",
-        dest="path_to_cp1_secret_key",
-        help="Path to counter-party one's secret_key.pem file.",
-        type=str,
-        )
+        _ARGS.add_argument(
+            "--cp1-secret-key-path",
+            default="E:\Blockchain\Casper\wallet_key\TreasuryAccount_secret_key.pem",
+            dest="path_to_cp1_secret_key",
+            help="Path to counter-party one's secret_key.pem file.",
+            type=str,
+            )
 
-    _ARGS.add_argument(
-        "--cp1-secret-key-type",
-        default="ED25519",
-        dest="type_of_cp1_secret_key",
-        help="Type of counter party one's secret key.",
-        type=str,
-        )
+        _ARGS.add_argument(
+            "--cp1-secret-key-type",
+            default="ED25519",
+            dest="type_of_cp1_secret_key",
+            help="Type of counter party one's secret key.",
+            type=str,
+            )
 
-    _ARGS.add_argument(
-        "--cp2-account-key-path",
-        default="E:\Blockchain\Casper\public_keys\omar_pb_key.txt",
-        dest="path_to_cp2_account_key",
-        help="Path to counter-party two's public_key_hex file.",
-        type=str,
-        )
-
-
-    _ARGS.add_argument(
-        "--node-host",
-        default="136.243.187.84",
-        dest="node_host",
-        help="Host address of target node.",
-        type=str,
-        )
-
-    _ARGS.add_argument(
-        "--node-port-rpc",
-        default="7777",
-        dest="node_port_rpc",
-        help="Node API JSON-RPC port.  Typically 7777 on most nodes.",
-        type=int,
-        )
-
-    _ARGS.add_argument(
-        "--chain",
-        default="casper-test",
-        dest="chain_name",
-        help="Name of target chain.",
-        type=str,
-        )
-
-    """
-    Put this line to get the args from the cmd
-    #args = _ARGS.parse_args()
-
-    Put this line to use argparse without the cmd
-    args = _ARGS.parse_args(["--cp1-secret-key-path", "E:\Blockchain\Casper\wallet_key\TreasuryAccount_secret_key.pem", "--cp1-secret-key-type", "ED25519", "--cp2-account-key-path", "E:\Blockchain\Casper\public_keys\omar_pb_key.txt", "--node-host","136.243.187.84", "--node-port-rpc","7777", "--chain", "casper-test"])
-    
-    """
-
-    args = _ARGS.parse_args(["--cp1-secret-key-path", "E:\Blockchain\Casper\wallet_key\TreasuryAccount_secret_key.pem", "--cp1-secret-key-type", "ED25519", "--cp2-account-key-path", "E:\Blockchain\Casper\public_keys\omar_pb_key.txt", "--node-host","136.243.187.84", "--node-port-rpc","7777", "--chain", "casper-test"])
-
-    client = _get_client(args)
-
-    #cp1 = Treasuery Account
-    cp1, cp2 = _get_counter_parties(args)
+        _ARGS.add_argument(
+            "--cp2-account-key-path",
+            default="E:\Blockchain\Casper\public_keys\omar_pb_key.txt",
+            dest="path_to_cp2_account_key",
+            help="Path to counter-party two's public_key_hex file.",
+            type=str,
+            )
 
 
-    # Set deploy.
-    deploy: Deploy = _get_deploy(args, cp1, cp2)
+        _ARGS.add_argument(
+            "--node-host",
+            default="136.243.187.84",
+            dest="node_host",
+            help="Host address of target node.",
+            type=str,
+            )
+
+        _ARGS.add_argument(
+            "--node-port-rpc",
+            default="7777",
+            dest="node_port_rpc",
+            help="Node API JSON-RPC port.  Typically 7777 on most nodes.",
+            type=int,
+            )
+
+        _ARGS.add_argument(
+            "--chain",
+            default="casper-test",
+            dest="chain_name",
+            help="Name of target chain.",
+            type=str,
+            )
+
+        """
+        Put this line to get the args from the cmd
+        #args = _ARGS.parse_args()
+
+        Put this line to use argparse without the cmd
+        args = _ARGS.parse_args(["--cp1-secret-key-path", "E:\Blockchain\Casper\wallet_key\TreasuryAccount_secret_key.pem", "--cp1-secret-key-type", "ED25519", "--cp2-account-key-path", "E:\Blockchain\Casper\public_keys\omar_pb_key.txt", "--node-host","136.243.187.84", "--node-port-rpc","7777", "--chain", "casper-test"])
+        
+        """
+
+        args = _ARGS.parse_args(["--cp1-secret-key-path", "E:\Blockchain\Casper\wallet_key\TreasuryAccount_secret_key.pem", "--cp1-secret-key-type", "ED25519", "--cp2-account-key-path", "E:\Blockchain\Casper\public_keys\omar_pb_key.txt", "--node-host","136.243.187.84", "--node-port-rpc","7777", "--chain", "casper-test"])
+
+        client = _get_client(args)
+
+        #cp1 = Treasuery Account
+        cp1, cp2 = _get_counter_parties(args)
 
 
-    # Approve deploy.
-    print("deploy approve started.")
-    deploy.approve(cp1)
-    print("deploy approve finished.")
+        # Set deploy.
+        deploy: Deploy = _get_deploy(args, cp1, cp2)
 
-    # Dispatch deploy to a node.
-    print("send deploy started.")
-    client.send_deploy(deploy)
-    print("send deploy finished.")
 
-    print(f"Deploy dispatched to node [{args.node_host}]: {deploy.hash.hex()}")
+        # Approve deploy.
+        print("deploy approve started.")
+        deploy.approve(cp1)
+        print("deploy approve finished.")
 
-    return HttpResponse("<h1>Done</h1>")
+        # Dispatch deploy to a node.
+        print("send deploy started.")
+        client.send_deploy(deploy)
+        print("send deploy finished.")
+
+        print(f"Deploy dispatched to node [{args.node_host}]: {deploy.hash.hex()}")
+
+        return HttpResponse("<h1>Done</h1>")
+    else:
+        if(qr_code in qr_code_list and qr_code_dict[qr_code] == 1):
+            print("This QR code has already been used.")
+            return HttpResponse("<h1>Invalid QR code</h1>")
+        else:
+            print("Invalid QR code")
+            return HttpResponse("<h1>Invalid QR code</h1>")
+
 
 
